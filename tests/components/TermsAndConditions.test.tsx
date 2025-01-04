b24adcd7cbb967d962c250c58dc12b028e6e290b
Shadman Ahmed <Shadifmg@gmail.com>
2025-01-04 15:55:50 -0600

    feat: added TermsAndConditions to the playground, and added a style to disable (grey out) submit button when terms and conditions check box is not clicked. set up test page to add to for terms and conditions page

---
+++ tests/components/TermsAndConditions.test.tsx
@@ -0,0 +1,8 @@
+import { render, screen } from '@testing-library/react'
+import TermsAndConditions from '../../src/components/TermsAndConditions'
+
+describe('TermsAndConditions', () => {
+    it('should render a checkbox', () => {
+
+    });
+});
