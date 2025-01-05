056bf9450ec6fdb5677adffb93e9f26ad7bbc759
Shadman Ahmed <Shadifmg@gmail.com>
2025-01-04 22:35:14 -0600

    feat: added paragraph longer than 255 char in playground page with ExpandableText rendered. Did integration tests for the rendering of the text and Show More button render if more than 255 char. Added tests for user interactions for the show more and show less button

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -2,10 +2,7 @@ import ExpandableText from "../components/ExpandableText";
 
 const PlaygroundPage = () => {
   return (
-    <ExpandableText
-      text="  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit exercitationem modi blanditiis dolores error minus ipsam incidunt eius quis explicabo amet maiores tempora animi aliquam, labore corrupti? Aut, culpa labore quisquam magnam aliquam fugit suscipit nisi cum cupiditate eius aliquid dolorem placeat odit, ea sed illum est esse repellat deleniti enim tempore velit debitis perferendis consequatur? Earum sequi ducimus, incidunt et mollitia hic corporis, nemo aliquid at recusandae eveniet debitis numquam ad, reprehenderit illo natus in. Molestias, odio molestiae fugiat ratione eos hic provident natus dolore doloribus rem itaque repellat sapiente soluta nam consequuntur voluptas debitis quisquam, aliquam omnis exercitationem!
-"
-    />
+    <ExpandableText text="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Velit exercitationem modi blanditiis dolores error minus ipsam incidunt eius quis explicabo amet maiores tempora animi aliquam, labore corrupti? Aut, culpa labore quisquam magnam aliquam fugit suscipit nisi cum cupiditate eius aliquid dolorem placeat odit, ea sed illum est esse repellat deleniti enim tempore velit debitis perferendis consequatur? Earum sequi ducimus, incidunt et mollitia hic corporis, nemo aliquid at recusandae eveniet debitis numquam ad, reprehenderit illo natus in. Molestias, odio molestiae fugiat ratione eos hic provident natus dolore doloribus rem itaque repellat sapiente soluta nam consequuntur voluptas debitis quisquam, aliquam omnis exercitationem!" />
   );
 };
 
